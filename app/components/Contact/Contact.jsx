83256386c6b701293bc3c6759eb70e62f1fcba8d
Ludyne <ludivine.veloso@laposte.net>
2025-03-10 21:37:33 +0100

    ajout lien CV et qlq modifications

--- app/components/Contact/Contact.jsx
+++ app/components/Contact/Contact.jsx
@@ -2,23 +2,34 @@ import { LuMail } from "react-icons/lu";
 import { MdFileDownload } from "react-icons/md";
 
 export default function Contact() {
-    return (
-        <section className="container">
-            <h3 className="font-semibold text-3xl pt-6 mb-6" id="contact">Contact</h3>
-            <p>Une opportunité d&apos;alternance ?</p>
-            <div className="flex flex-col gap-8 my-8">
-                <div className="flex flex-row gap-x-3">
-                    <LuMail className="text-[24px] aria-hidden"></LuMail>
-                    <p className="underline"><span className="blockspam aria-hidden hidden">Span block</span>ludivine.veloso@laposte.net</p>
-                </div>
-                <div>
-                <div className=" border border-gray-700 bg-[#060a12] rounded-full hover:scale-110 transition-all flex flex-row p-2  cursor-pointer w-[112px]">
-                    <MdFileDownload className="text-[24px] text-white aria-hidden ">
-                    </MdFileDownload>
-                    <a id="cv" href="https://drive.google.com/file/d/1wJgsG-5BMWazsAWnZqepfC91OfsIIk6X/view?usp=sharing" target="_blank" className=" text-white">Mon CV</a>
-                </div>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className="container">
+      <h3 className="font-semibold text-3xl pt-6 mb-6" id="contact">
+        Contact
+      </h3>
+      <p>Une opportunité d&apos;alternance ?</p>
+      <div className="flex flex-col gap-8 my-8">
+        <div className="flex flex-row gap-x-3">
+          <LuMail className="text-[24px] aria-hidden"></LuMail>
+          <p className="underline">
+            <span className="blockspam aria-hidden hidden">Span block</span>
+            ludivine.veloso@laposte.net
+          </p>
+        </div>
+        <div>
+          <div className=" border border-gray-700 bg-[#060a12] rounded-full hover:scale-110 transition-all flex flex-row p-2  cursor-pointer w-[112px]">
+            <MdFileDownload className="text-[24px] text-white aria-hidden "></MdFileDownload>
+            <a
+              id="cv"
+              href="https://drive.google.com/file/d/1JzIwZ2u2lbaxPg6l-JypGqRZFDfuK-JQ/view?usp=sharing"
+              target="_blank"
+              className=" text-white"
+            >
+              Mon CV
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,9 +1,12 @@
 import { FaRegCopyright } from "react-icons/fa";
 
 export default function Footer() {
-    return (
-        <footer className="flex justify-center text-white p-[50px]">
-            <p className="inline-flex items-center"><FaRegCopyright className="not-sr-only"/> Portfolio -  Tous droits réservés - 2024</p>
-        </footer>
-    )
+  return (
+    <footer className="flex justify-center text-white p-[50px]">
+      <p className="inline-flex items-center">
+        <FaRegCopyright className="not-sr-only" /> Portfolio - Tous droits
+        réservés - 2025
+      </p>
+    </footer>
+  );
 }
